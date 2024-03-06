3c5f28655d2ca9707ddbb94edf6ad84463ef7e3c
MrAlvaroRamirez <mr.alvaro.ramirez@gmail.com>
2024-03-06 18:20:35 +0100

    Added native plugin build

---
+++ Build/Plugin/iOS_Plugin_Monitoring.framework/Headers/iOS_Plugin_Monitoring.h
@@ -0,0 +1,16 @@
+//
+//  iOS_Plugin_Monitoring.h
+//  iOS Plugin Monitoring
+//
+//  Created by Alvaro on 6/3/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for test.
+FOUNDATION_EXPORT double iOS_Plugin_MonitoringVersionNumber;
+
+//! Project version string for test.
+FOUNDATION_EXPORT const unsigned char iOS_Plugin_MonitoringVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <test/PublicHeader.h>
